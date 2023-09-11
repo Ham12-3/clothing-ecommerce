b5431ced3a0ee2d624c8a8d0cd69873a1d96df2c
Abdulhamid <mobolaji2309@gmail.com>
2023-09-11 14:45:12 +0100

    starting project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-600">Bazar E-commerce</h1>
+    </div>
+  );
+}
+
+export default App;
