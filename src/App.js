94e37157758859f293cc34029c94fc025274442d
Abdulhamid <mobolaji2309@gmail.com>
2023-09-12 07:12:13 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { ImGithub } from "react-icons/im";
-import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaYoutube,
+  FaHome,
+} from "react-icons/fa";
 import { logoLight, paymentLogo } from "../assets";
-
+import { MdLocationOn } from "react-icons/md";
+import { BsPersonFill, BsPaypal } from "react-icons/bs";
 const Footer = () => {
   return (
     <div className="bg-black text-[#949494] py-20 font-titleFont">
@@ -31,6 +38,49 @@ const Footer = () => {
             <p> Phone: 00968 2476821</p>
             <p>E-mail: bazar@gmail.com</p>
           </div>
+        </div>
+        <div>
+          <h2 className="text-2xl font-semibold text-whiite mb-4">Profile</h2>
+          <div className="flex flex-col gap-2 text-base">
+            <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+              {" "}
+              <span>
+                <BsPersonFill />
+              </span>{" "}
+              my account
+            </p>
+            <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+              {" "}
+              <span>
+                <BsPaypal />
+              </span>{" "}
+              checkout
+            </p>
+            <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+              {" "}
+              <span>
+                <FaHome />
+              </span>{" "}
+              order tracking
+            </p>
+            <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+              {" "}
+              <span>
+                <MdLocationOn />
+              </span>{" "}
+              help & support
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center">
+          <input
+            className="bg-transparent border px-4 py-2 text-sm "
+            placeholder="e-mail"
+            type="text"
+          />
+          <button className="text-sm border text-white border-t-0 hover:bg-gray-900 active:bg-white active:text-black">
+            Subscribe
+          </button>
         </div>
       </div>
     </div>
